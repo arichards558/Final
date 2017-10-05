5cbe9fab031d5fd53ca99609d5b8c8467f1e7dcb
iDubTheniStep <dp201524@students.davenport.k12.ia.us>
2017-10-05 09:26:42 -0500

    update

--- Final/src/Test/test.java
+++ Final/src/Test/test.java
@@ -1,12 +1,19 @@
 package Test;
 
+import java.io.*;
+import sun.audio.*;
+
 public class test
 
 {
 	public static void main(String[] args) 
+	throws Exception
 	{
-		
-		
-		
+		  String wav_file = "C:\\Users\\dp201524\\Desktop\\JavaGitHub\\Final\\Final\\src\\Test\\no.wav";
+		  
+		InputStream in = new FileInputStream(wav_file);
+		  
+		  AudioStream audio = new AudioStream(in);
+		  AudioPlayer.player.start(audio);
 	}
 }
